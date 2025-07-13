7af1201e1d94e8e1ca10156028f9326ba8af9218
Simon Knox <psimyn@gmail.com>
2025-07-13 21:19:18 +1000

    init alpine

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'tx6-cache-v1';
+const OFFLINE_URLS = [
+  '/',
+  'index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(OFFLINE_URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('index.html');
+        }
+      });
+    })
+  );
+});
