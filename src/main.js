b8f5ac84eb4e14d05690f44398245694c9eedd80
psimyn <psimyn@gmail.com>
2025-12-24 20:52:03 +1100

    better fullscreen button

--- src/main.js
+++ src/main.js
@@ -1029,8 +1029,14 @@ Alpine.data('tx6Controller', () => ({
     },
 
     toggleFullscreen() {
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
+        if (document.fullscreenElement) {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        } else {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            }
         }
     },
 
